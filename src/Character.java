044c8fdb8d5a4cb804ebf44c85bbc38623cc0240
DaleB34 <dalebolido@gmail.com>
2024-08-16 11:24:09 -0700

    Made instance variables and constructor

--- src/Character.java
+++ src/Character.java
@@ -1,4 +1,19 @@
 public class Character
 {
+    //instance variables
+    String name;
+    int money;
+    int attack;
+
+    // constructor
+    public Character(String name, int money, int attack) {
+        this.name = name;
+        this.money = money;
+        this.attack = attack;
+    }
+
+
+
+
 
 }
