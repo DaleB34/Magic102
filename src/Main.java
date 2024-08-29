507d9947d72ac6dfffba1215de24eeaa4ece8fc4
DaleB34 <dalebolido@gmail.com>
2024-08-29 13:23:05 -0700

    made gankMid method

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args)
     {
-        Bard bard = new Bard("Bard", 1000, 10, "Horn", 100);
+        Weapon horn = new Weapon("Meep", 45, 35, 30, 100);
+        Staff flow = new Staff("Staff of Flowing Water", 85, 30, 200, 400, "League", "Water");
+        Bard bard = new Bard("Bard", 1000, 10, horn, 100);
+        Bard poo = new Bard("Poo", 1000, 10, horn, 100);
 
         System.out.println(bard.toString());
+        flow.destroy();
     }
 }
