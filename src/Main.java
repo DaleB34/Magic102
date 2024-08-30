7da193e1bc69f36e548608920f69823c077b04f6
DaleB34 <dalebolido@gmail.com>
2024-08-30 13:14:07 -0700

    Instantiated some Weapon and Staff objects for testing

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args)
     {
+        //weapons
         Weapon horn = new Weapon("Meep", 45, 35, 30, 100);
-        Staff flow = new Staff("Staff of Flowing Water", 85, 30, 200, 400, "League", "Water");
-        Bard bard = new Bard("Bard", 1000, 10, horn, 100);
-        Bard poo = new Bard("Poo", 1000, 10, horn, 100);
+        Staff flow = new Staff("Staff of Flowing Water", 50, 0, 200, 400, "League", "Water");
+        Weapon bfs = new Weapon("B.F. Sword", 40, 0, 0, 0);
+        Staff voidS = new Staff("Void Staff", 65, 0, 200, 400, "League", "Void");
 
-        System.out.println(bard.toString());
-        flow.destroy();
+
+
     }
 }
